9e24ddd98368fe84226f9a70dcaab030912bb44e
Ajay Singh <aos2500@g.rit.edu>
2024-04-05 19:38:37 -0400

    Updated background code

--- flicks/src/FrontBackground.js
+++ flicks/src/FrontBackground.js
@@ -3,8 +3,14 @@ import './FrontBackground.css';
 
 const FrontBackground = () => {
     return (
-        <div className="background-container">
-            {/* Background image styles */}
+        <div className="front-background-container">
+            {/* Background image with welcome text overlay */}
+            <div className="background-image">
+                <div className="overlay-text">
+                    <h1>Welcome To Flicks</h1>
+                    <p>Scroll Down To Start</p>
+                </div>
+            </div>
         </div>
     );
 };
