28c6e95863fb04dda30fd847ce563b2a61a26b2a
Ajay Singh <aos2500@g.rit.edu>
2024-04-05 19:21:36 -0400

    previous state

--- flicks/src/FrontPage.js
+++ flicks/src/FrontPage.js
@@ -4,19 +4,23 @@ import './FrontBackground.css';
 import PhotoDisplayer from './PhotoDisplayer';
 import AboutMe from './AboutMe';
 import Socials from './Socials';
-import './FrontPage.css'; // Import custom CSS for FrontPage styling
+import './FrontPage.css';
 
 const FrontPage = () => {
     return (
         <div>
             <FrontBackground />
             <div className="content-container">
-                <h1 className="title">Welcome To Flicks</h1> {/* Apply title styles */}
+            <h1 className="title">Welcome To Flicks</h1>
+            </div>
+            
+
+            <div className="content-container">
                 <section>
                     <h2>My Work</h2>
                     <p>Here you can see some of my favorite shots.</p>
+                    <PhotoDisplayer /> {}
                 </section>
-                <PhotoDisplayer /> {/* Display photos further down the page */}
                 <section>
                     <h2>About Me</h2>
                     <p>I am a photographer based in Rochester, NY</p>
