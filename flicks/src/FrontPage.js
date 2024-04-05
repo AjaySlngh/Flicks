4e8eefb606c31d2460810741f4d4e8d5b20ec61b
Ajay Singh <aos2500@g.rit.edu>
2024-04-05 19:31:02 -0400

    somewhere closer

--- flicks/src/FrontPage.js
+++ flicks/src/FrontPage.js
@@ -10,22 +10,22 @@ const FrontPage = () => {
     return (
         <div>
             <FrontBackground />
-            <div className="content-container">
-            <h1 className="title">Welcome To Flicks</h1>
+            <div className="background-overlay">
+                <h1>Welcome To Flicks</h1>
             </div>
-            
 
             <div className="content-container">
-                <section>
-                    <h2>My Work</h2>
-                    <p>Here you can see some of my favorite shots.</p>
-                    <PhotoDisplayer /> {}
-                </section>
+
+                <div className="photo-displayer-container">
+                    <PhotoDisplayer />
+                </div>
+
                 <section>
                     <h2>About Me</h2>
                     <p>I am a photographer based in Rochester, NY</p>
                     <AboutMe />
                 </section>
+
                 <section>
                     <h2>Contact</h2>
                     <p>Want to get in touch? Reach out on my social channels or send me an email.</p>
