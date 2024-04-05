ffd58d05f534b1245ede0b6ad1f7866a7370f033
Ajay Singh <aos2500@g.rit.edu>
2024-04-05 17:17:09 -0400

    making background image work

---
+++ flicks/src/FrontBackground.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FrontBackground.css';
+
+const FrontBackground = () => {
+    return (
+        <div className="background-container">
+            {/* Background image styles */}
+        </div>
+    );
+};
+
+export default FrontBackground;
--- flicks/src/FrontPage.js
+++ flicks/src/FrontPage.js
@@ -1,49 +1,35 @@
 import React from 'react';
+import FrontBackground from './FrontBackground';
+import './FrontBackground.css';
 import PhotoDisplayer from './PhotoDisplayer';
 import AboutMe from './AboutMe';
 import Socials from './Socials';
-import './FrontPage.css';
-import { useEffect } from 'react';
+import './FrontPage.css'; // Import custom CSS for FrontPage styling
 
 const FrontPage = () => {
-    useEffect(() => {
-        const handleScroll = () => {
-            const frontPageContainer = document.getElementById('frontPageContainer');
-            if (frontPageContainer) {
-                const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                if (scrollTop > 0) {
-                    frontPageContainer.classList.add('neutral-background');
-                } else {
-                    frontPageContainer.classList.remove('neutral-background');
-                }
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     return (
         <div>
-            <section> 
-                <h2 style={{ textAlign: 'center' }}>My Work</h2>
-                <p>Here you can see some of my favorite shots.</p>
-                <PhotoDisplayer />
-            </section>
-            <section>
-                <h2 style={{ textAlign: 'center' }}>About Me</h2>
-                <p>I am a photographer based in Rochester, NY</p>
-                <AboutMe />
-            </section>
-            <section>
-                <h2 style={{ textAlign: 'center' }}>Contact</h2>
-                <p>Want to get in touch? Reach out on my social channels or send me an email.</p>
-                <Socials />
-            </section>
+            <FrontBackground />
+            <div className="content-container">
+                <h1 className="title">Welcome To Flicks</h1> {/* Apply title styles */}
+                <section>
+                    <h2>My Work</h2>
+                    <p>Here you can see some of my favorite shots.</p>
+                </section>
+                <PhotoDisplayer /> {/* Display photos further down the page */}
+                <section>
+                    <h2>About Me</h2>
+                    <p>I am a photographer based in Rochester, NY</p>
+                    <AboutMe />
+                </section>
+                <section>
+                    <h2>Contact</h2>
+                    <p>Want to get in touch? Reach out on my social channels or send me an email.</p>
+                    <Socials />
+                </section>
+            </div>
         </div>
     );
-}
+};
 
 export default FrontPage;
