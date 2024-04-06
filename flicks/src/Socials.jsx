fc3a3731e0263e8b0a768bbbb247ad8982fa4026
Ajay Singh <aos2500@g.rit.edu>
2024-04-06 19:30:26 -0400

    made Socials.jsx look better by adding icons that are clickable and send you to their respective socials, and a CSS file for it.

--- flicks/src/Socials.jsx
+++ flicks/src/Socials.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import './Socials.css';
 
-class Socials extends React.Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <p> Email: ajaysingh7161@gmail.com </p>
-                    <a href="https://www.instagram.com/floppyflicks/">Instagram</a>
-                </div>
+const Socials = () => {
+    return (
+        <div className="socials-container">
+            <div className="social-icon gmail-icon">
+                <a href="mailto:ajaysingh7161@gmail.com">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/2560px-Gmail_icon_%282020%29.svg.png" alt="Email" />
+                </a>
             </div>
-        );
-    }
-}
+            <div className="social-icon">
+                <a href="https://www.instagram.com/floppyflicks/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1024px-Instagram-Icon.png" alt="Instagram" />
+                </a>
+            </div>
+        </div>
+    );
+};
 
 export default Socials;
